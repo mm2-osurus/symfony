ecd5382ae1c3801abfb2681274a3ffd5eda026f1
mm2-osurus <mm2.osurus@gmail.com>
2012-12-25 23:12:26 +0100

    Tuto Bundle
    Premier tuto sur les bundles
    
    Signed-off-by: mm2-osurus <mm2.osurus@gmail.com>

---
+++ src/Tuto/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tuto\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('HelloBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
